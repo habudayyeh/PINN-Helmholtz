4096de1e2f8e8ace352dab6b56419b7208f7202f
habudayyeh <abudayyehbus@gmail.com>
2025-04-29 09:13:12 -0500

    Initial Commit

---
+++ trainhelmholtz.py
@@ -0,0 +1,48 @@
+# Enable import from parent package
+import sys
+import os
+sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+import utils, modules, training
+
+from torch.utils.data import DataLoader
+
+
+logging_root= './logs'
+experiment_name='test'
+batch_size = 32
+lr = 2e-5
+num_epochs = 2000
+epochs_til_ckpt = 1000
+steps_til_summary = 100
+activation = 'sine'
+mode = 'mlp'
+velocity = 'uniform'
+clip_grad = 0.0
+use_lbfgs = False
+checkpoint_path = None
+
+# if we have a velocity perturbation, offset the source
+if velocity!='uniform':
+    source_coords = [-0.35, 0.]
+else:
+    source_coords = [0., 0.]
+
+dataset = utils.SingleHelmholtzSource(sidelength=100, velocity=velocity, source_coords=source_coords)
+
+dataloader = DataLoader(dataset, shuffle=True, batch_size=batch_size, num_workers=0)
+
+# Define the model.
+model = modules.SingleBVPNet(out_features=2, type=activation, mode=mode, final_layer_factor=1.)
+
+# model.cuda()
+
+# Define the loss
+loss_fn = utils.helmholtz_pml
+summary_fn = utils.write_helmholtz_summary
+
+root_path = os.path.join(logging_root, experiment_name)
+
+training.train(model=model, train_dataloader=dataloader, epochs=num_epochs, lr=lr,
+               steps_til_summary=steps_til_summary, epochs_til_checkpoint=epochs_til_ckpt,
+               model_dir=root_path, loss_fn=loss_fn, summary_fn=summary_fn, clip_grad=clip_grad,
+               use_lbfgs=use_lbfgs)
---
+++ utils.py
@@ -0,0 +1,326 @@
+import os
+import numpy as np
+import scipy.special
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.utils import make_grid
+
+import modules
+
+def cond_mkdir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def get_mgrid(sidelen, dim=2):
+    '''Generates a flattened grid of (x,y,...) coordinates in a range of -1 to 1.'''
+    if isinstance(sidelen, int):
+        sidelen = dim * (sidelen,)
+
+    if dim == 2:
+        pixel_coords = np.stack(np.mgrid[:sidelen[0], :sidelen[1]], axis=-1)[None, ...].astype(np.float32)
+        pixel_coords[0, :, :, 0] = pixel_coords[0, :, :, 0] / (sidelen[0] - 1)
+        pixel_coords[0, :, :, 1] = pixel_coords[0, :, :, 1] / (sidelen[1] - 1)
+    elif dim == 3:
+        pixel_coords = np.stack(np.mgrid[:sidelen[0], :sidelen[1], :sidelen[2]], axis=-1)[None, ...].astype(np.float32)
+        pixel_coords[..., 0] = pixel_coords[..., 0] / max(sidelen[0] - 1, 1)
+        pixel_coords[..., 1] = pixel_coords[..., 1] / (sidelen[1] - 1)
+        pixel_coords[..., 2] = pixel_coords[..., 2] / (sidelen[2] - 1)
+    else:
+        raise NotImplementedError('Not implemented for dim=%d' % dim)
+
+    pixel_coords -= 0.5
+    pixel_coords *= 2.
+    pixel_coords = torch.Tensor(pixel_coords).view(-1, dim)
+    return pixel_coords
+
+def lin2img(tensor, image_resolution=None):
+    batch_size, num_samples, channels = tensor.shape
+    if image_resolution is None:
+        width = np.sqrt(num_samples).astype(int)
+        height = width
+    else:
+        height = image_resolution[0]
+        width = image_resolution[1]
+
+    return tensor.permute(0, 2, 1).view(batch_size, channels, height, width)
+
+def gaussian(x, mu=[0, 0], sigma=1e-4, d=2):
+    x = x.numpy()
+    if isinstance(mu, torch.Tensor):
+        mu = mu.numpy()
+
+    q = -0.5 * ((x - mu) ** 2).sum(1)
+    return torch.from_numpy(1 / np.sqrt(sigma ** d * (2 * np.pi) ** d) * np.exp(q / sigma)).float()
+
+class SingleHelmholtzSource(Dataset):
+    def __init__(self, sidelength, velocity='uniform', source_coords=[0., 0.]):
+        super().__init__()
+        torch.manual_seed(0)
+
+        self.sidelength = sidelength
+        self.mgrid = get_mgrid(self.sidelength).detach()
+        self.velocity = velocity
+        self.wavenumber = 20.
+
+        self.N_src_samples = 100
+        self.sigma = 1e-4
+        self.source = torch.Tensor([1.0, 1.0]).view(-1, 2)
+        self.source_coords = torch.tensor(source_coords).view(-1, 2)
+
+        # For reference: this derives the closed-form solution for the inhomogenous Helmholtz equation.
+        square_meshgrid = lin2img(self.mgrid[None, ...]).numpy()
+        x = square_meshgrid[0, 0, ...]
+        y = square_meshgrid[0, 1, ...]
+
+        # Specify the source.
+        source_np = self.source.numpy()
+        hx = hy = 2 / self.sidelength
+        field = np.zeros((sidelength, sidelength)).astype(np.complex64)
+        for i in range(source_np.shape[0]):
+            x0 = self.source_coords[i, 0].numpy()
+            y0 = self.source_coords[i, 1].numpy()
+            s = source_np[i, 0] + 1j * source_np[i, 1]
+
+            hankel = scipy.special.hankel2(0, self.wavenumber * np.sqrt((x - x0) ** 2 + (y - y0) ** 2) + 1e-6)
+            field += 0.25j * hankel * s * hx * hy
+
+        field_r = torch.from_numpy(np.real(field).reshape(-1, 1))
+        field_i = torch.from_numpy(np.imag(field).reshape(-1, 1))
+        self.field = torch.cat((field_r, field_i), dim=1)
+
+    def __len__(self):
+        return 1
+
+    def get_squared_slowness(self, coords):
+        if self.velocity == 'square':
+            squared_slowness = torch.zeros_like(coords)
+            perturbation = 2.
+            mask = (torch.abs(coords[..., 0]) < 0.3) & (torch.abs(coords[..., 1]) < 0.3)
+            squared_slowness[..., 0] = torch.where(mask, 1. / perturbation ** 2 * torch.ones_like(mask.float()),
+                                                   torch.ones_like(mask.float()))
+        elif self.velocity == 'circle':
+            squared_slowness = torch.zeros_like(coords)
+            perturbation = 2.
+            mask = (torch.sqrt(coords[..., 0] ** 2 + coords[..., 1] ** 2) < 0.1)
+            squared_slowness[..., 0] = torch.where(mask, 1. / perturbation ** 2 * torch.ones_like(mask.float()),
+                                                   torch.ones_like(mask.float()))
+
+        else:
+            squared_slowness = torch.ones_like(coords)
+            squared_slowness[..., 1] = 0.
+
+        return squared_slowness
+
+    def __getitem__(self, idx):
+        # indicate where border values are
+        coords = torch.zeros(self.sidelength ** 2, 2).uniform_(-1., 1.)
+        source_coords_r = 5e2 * self.sigma * torch.rand(self.N_src_samples, 1).sqrt()
+        source_coords_theta = 2 * np.pi * torch.rand(self.N_src_samples, 1)
+        source_coords_x = source_coords_r * torch.cos(source_coords_theta) + self.source_coords[0, 0]
+        source_coords_y = source_coords_r * torch.sin(source_coords_theta) + self.source_coords[0, 1]
+        source_coords = torch.cat((source_coords_x, source_coords_y), dim=1)
+
+        # Always include coordinates where source is nonzero
+        coords[-self.N_src_samples:, :] = source_coords
+
+        # We use the value "zero" to encode "no boundary constraint at this coordinate"
+        boundary_values = self.source * gaussian(coords, mu=self.source_coords, sigma=self.sigma)[:, None]
+        boundary_values[boundary_values < 1e-5] = 0.
+
+        # specify squared slowness
+        squared_slowness = self.get_squared_slowness(coords)
+        squared_slowness_grid = self.get_squared_slowness(self.mgrid)[:, 0, None]
+
+        return {'coords': coords}, {'source_boundary_values': boundary_values, 'gt': self.field,
+                                    'squared_slowness': squared_slowness,
+                                    'squared_slowness_grid': squared_slowness_grid,
+                                    'wavenumber': self.wavenumber}
+
+
+def helmholtz_pml(model_output, gt):
+    source_boundary_values = gt['source_boundary_values']
+
+    if 'rec_boundary_values' in gt:
+        rec_boundary_values = gt['rec_boundary_values']
+
+    wavenumber = gt['wavenumber'].float()
+    x = model_output['model_in']  # (meta_batch_size, num_points, 2)
+    y = model_output['model_out']  # (meta_batch_size, num_points, 2)
+    squared_slowness = gt['squared_slowness'].repeat(1, 1, y.shape[-1] // 2)
+    batch_size = x.shape[1]
+
+    full_waveform_inversion = False
+    if 'pretrain' in gt:
+        pred_squared_slowness = y[:, :, -1] + 1.
+        if torch.all(gt['pretrain'] == -1):
+            full_waveform_inversion = True
+            pred_squared_slowness = torch.clamp(y[:, :, -1], min=-0.999) + 1.
+            squared_slowness_init = torch.stack((torch.ones_like(pred_squared_slowness),
+                                                 torch.zeros_like(pred_squared_slowness)), dim=-1)
+            squared_slowness = torch.stack((pred_squared_slowness, torch.zeros_like(pred_squared_slowness)), dim=-1)
+            squared_slowness = torch.where((torch.abs(x[..., 0, None]) > 0.75) | (torch.abs(x[..., 1, None]) > 0.75),
+                                           squared_slowness_init, squared_slowness)
+        y = y[:, :, :-1]
+
+    du, status = jacobian(y, x)
+    dudx1 = du[..., 0]
+    dudx2 = du[..., 1]
+
+    a0 = 5.0
+
+    # let pml extend from -1. to -1 + Lpml and 1 - Lpml to 1.0
+    Lpml = 0.5
+    dist_west = -torch.clamp(x[..., 0] + (1.0 - Lpml), max=0)
+    dist_east = torch.clamp(x[..., 0] - (1.0 - Lpml), min=0)
+    dist_south = -torch.clamp(x[..., 1] + (1.0 - Lpml), max=0)
+    dist_north = torch.clamp(x[..., 1] - (1.0 - Lpml), min=0)
+
+    sx = wavenumber * a0 * ((dist_west / Lpml) ** 2 + (dist_east / Lpml) ** 2)[..., None]
+    sy = wavenumber * a0 * ((dist_north / Lpml) ** 2 + (dist_south / Lpml) ** 2)[..., None]
+
+    ex = torch.cat((torch.ones_like(sx), -sx / wavenumber), dim=-1)
+    ey = torch.cat((torch.ones_like(sy), -sy / wavenumber), dim=-1)
+
+    A = modules.compl_div(ey, ex).repeat(1, 1, dudx1.shape[-1] // 2)
+    B = modules.compl_div(ex, ey).repeat(1, 1, dudx1.shape[-1] // 2)
+    C = modules.compl_mul(ex, ey).repeat(1, 1, dudx1.shape[-1] // 2)
+
+    a, _ = jacobian(modules.compl_mul(A, dudx1), x)
+    b, _ = jacobian(modules.compl_mul(B, dudx2), x)
+
+    a = a[..., 0]
+    b = b[..., 1]
+    c = modules.compl_mul(modules.compl_mul(C, squared_slowness), wavenumber ** 2 * y)
+
+    diff_constraint_hom = a + b + c
+    diff_constraint_on = torch.where(source_boundary_values != 0.,
+                                     diff_constraint_hom - source_boundary_values,
+                                     torch.zeros_like(diff_constraint_hom))
+    diff_constraint_off = torch.where(source_boundary_values == 0.,
+                                      diff_constraint_hom,
+                                      torch.zeros_like(diff_constraint_hom))
+    if full_waveform_inversion:
+        data_term = torch.where(rec_boundary_values != 0, y - rec_boundary_values, torch.Tensor([0.]).cuda())
+    else:
+        data_term = torch.Tensor([0.])
+
+        if 'pretrain' in gt:  # we are not trying to solve for velocity
+            data_term = pred_squared_slowness - squared_slowness[..., 0]
+
+    return {'diff_constraint_on': torch.abs(diff_constraint_on).sum() * batch_size / 1e3,
+            'diff_constraint_off': torch.abs(diff_constraint_off).sum(),
+            'data_term': torch.abs(data_term).sum() * batch_size / 1}
+
+
+def write_helmholtz_summary(model, model_input, gt, model_output, writer, total_steps, prefix='train_'):
+    sl = 256
+    coords = get_mgrid(sl)[None,...].cuda()
+
+    def scale_percentile(pred, min_perc=1, max_perc=99):
+        min = np.percentile(pred.cpu().numpy(),1)
+        max = np.percentile(pred.cpu().numpy(),99)
+        pred = torch.clamp(pred, min, max)
+        return (pred - min) / (max-min)
+
+    with torch.no_grad():
+        if 'coords_sub' in model_input:
+            summary_model_input = {'coords':coords.repeat(min(2, model_input['coords_sub'].shape[0]),1,1)}
+            summary_model_input['coords_sub'] = model_input['coords_sub'][:2,...]
+            summary_model_input['img_sub'] = model_input['img_sub'][:2,...]
+            pred = model(summary_model_input)['model_out']
+        else:
+            pred = model({'coords': coords})['model_out']
+
+        if 'pretrain' in gt:
+            gt['squared_slowness_grid'] = pred[...,-1, None].clone() + 1.
+            if torch.all(gt['pretrain'] == -1):
+                gt['squared_slowness_grid'] = torch.clamp(pred[...,-1, None].clone(), min=-0.999) + 1.
+                gt['squared_slowness_grid'] = torch.where((torch.abs(coords[...,0,None]) > 0.75) | (torch.abs(coords[...,1,None]) > 0.75),
+                                            torch.ones_like(gt['squared_slowness_grid']),
+                                            gt['squared_slowness_grid'])
+            pred = pred[...,:-1]
+
+        pred = lin2img(pred)
+
+        pred_cmpl = pred[...,0::2,:,:].cpu().numpy() + 1j * pred[...,1::2,:,:].cpu().numpy()
+        pred_angle = torch.from_numpy(np.angle(pred_cmpl))
+        pred_mag = torch.from_numpy(np.abs(pred_cmpl))
+
+        min_max_summary(prefix + 'coords', model_input['coords'], writer, total_steps)
+        min_max_summary(prefix + 'pred_real', pred[..., 0::2, :, :], writer, total_steps)
+        min_max_summary(prefix + 'pred_abs', torch.sqrt(pred[..., 0::2, :, :]**2 + pred[..., 1::2, :, :]**2), writer, total_steps)
+        min_max_summary(prefix + 'squared_slowness', gt['squared_slowness_grid'], writer, total_steps)
+
+        pred = scale_percentile(pred)
+        pred_angle = scale_percentile(pred_angle)
+        pred_mag = scale_percentile(pred_mag)
+
+        pred = pred.permute(1, 0, 2, 3)
+        pred_mag = pred_mag.permute(1, 0, 2, 3)
+        pred_angle = pred_angle.permute(1, 0, 2, 3)
+
+    writer.add_image(prefix + 'pred_real', make_grid(pred[0::2, :, :, :], scale_each=False, normalize=True),
+                     global_step=total_steps)
+    writer.add_image(prefix + 'pred_imaginary', make_grid(pred[1::2, :, :, :], scale_each=False, normalize=True),
+                     global_step=total_steps)
+    writer.add_image(prefix + 'pred_angle', make_grid(pred_angle, scale_each=False, normalize=True),
+                     global_step=total_steps)
+    writer.add_image(prefix + 'pred_mag', make_grid(pred_mag, scale_each=False, normalize=True),
+                     global_step=total_steps)
+
+    if 'gt' in gt:
+        gt_field = lin2img(gt['gt'])
+        gt_field_cmpl = gt_field[...,0,:,:].cpu().numpy() + 1j * gt_field[...,1,:,:].cpu().numpy()
+        gt_angle = torch.from_numpy(np.angle(gt_field_cmpl))
+        gt_mag = torch.from_numpy(np.abs(gt_field_cmpl))
+
+        gt_field = scale_percentile(gt_field)
+        gt_angle = scale_percentile(gt_angle)
+        gt_mag = scale_percentile(gt_mag)
+
+        writer.add_image(prefix + 'gt_real', make_grid(gt_field[...,0,:,:], scale_each=False, normalize=True),
+                         global_step=total_steps)
+        writer.add_image(prefix + 'gt_imaginary', make_grid(gt_field[...,1,:,:], scale_each=False, normalize=True),
+                         global_step=total_steps)
+        writer.add_image(prefix + 'gt_angle', make_grid(gt_angle, scale_each=False, normalize=True),
+                         global_step=total_steps)
+        writer.add_image(prefix + 'gt_mag', make_grid(gt_mag, scale_each=False, normalize=True),
+                         global_step=total_steps)
+        min_max_summary(prefix + 'gt_real', gt_field[..., 0, :, :], writer, total_steps)
+
+    velocity = torch.sqrt(1/lin2img(gt['squared_slowness_grid']))[:1]
+    min_max_summary(prefix + 'velocity', velocity[..., 0, :, :], writer, total_steps)
+    velocity = scale_percentile(velocity)
+    writer.add_image(prefix + 'velocity', make_grid(velocity[...,0,:,:], scale_each=False, normalize=True),
+                     global_step=total_steps)
+
+    if 'squared_slowness_grid' in gt:
+        writer.add_image(prefix + 'squared_slowness', make_grid(lin2img(gt['squared_slowness_grid'])[:2,:1],
+                                                                scale_each=False, normalize=True),
+                         global_step=total_steps)
+
+    if 'img_sub' in model_input:
+        writer.add_image(prefix + 'img', make_grid(lin2img(model_input['img_sub'])[:2,:1],
+                                                                scale_each=False, normalize=True),
+                         global_step=total_steps)
+
+
+def jacobian(y, x):
+    ''' jacobian of y wrt x '''
+    meta_batch_size, num_observations = y.shape[:2]
+    jac = torch.zeros(meta_batch_size, num_observations, y.shape[-1], x.shape[-1]).to(y.device) # (meta_batch_size*num_points, 2, 2)
+    for i in range(y.shape[-1]):
+        # calculate dydx over batches for each feature value of y
+        y_flat = y[...,i].view(-1, 1)
+        jac[:, :, i, :] = torch.autograd.grad(y_flat, x, torch.ones_like(y_flat), create_graph=True)[0]
+
+    status = 0
+    if torch.any(torch.isnan(jac)):
+        status = -1
+
+    return jac, status
+
+def min_max_summary(name, tensor, writer, total_steps):
+    writer.add_scalar(name + '_min', tensor.min().detach().cpu().numpy(), total_steps)
+    writer.add_scalar(name + '_max', tensor.max().detach().cpu().numpy(), total_steps)
